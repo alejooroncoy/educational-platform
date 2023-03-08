2364e752d3d85cda84187cf7346bfea71610df80
alejooroncoy <adoa2705@gmail.com>
2023-03-08 11:35:45 -0500

    feat: Add routes 👨‍💻✨

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,5 +1,21 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "../layouts/Layout";
+import Courses from "../pages/Courses";
+
 const App = () => {
-  return <div>App</div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route path="courses" element={<Courses />} />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
