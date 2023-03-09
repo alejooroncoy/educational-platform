fb8a1b8fcd5e4ea9c297df81513e2066aa1d1ebb
alejooroncoy <adoa2705@gmail.com>
2023-03-09 11:38:22 -0500

    feat: Update pages, Home and Courses 👨‍💻✨

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,5 +1,22 @@
+import { redirect, useLoaderData } from "react-router-dom";
+import CoursesContent from "../components/Courses/CoursesContent";
+
+export const loaderCourses = (params) => {
+  const { searchParams } = new URL(params.request.url);
+  const category = searchParams.get("category");
+  if (!category) return redirect(`${params.request.url}?category=all`);
+  return {
+    category,
+  };
+};
+
 const Courses = () => {
-  return <main className="main">Courses</main>;
+  const { category } = useLoaderData();
+  return (
+    <main className="main">
+      <CoursesContent category={category} />
+    </main>
+  );
 };
 
 export default Courses;
