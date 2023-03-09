3b7a239ad741137a75072c03b4e132bb175aa05e
alejooroncoy <adoa2705@gmail.com>
2023-03-09 11:39:42 -0500

    feat: Add getStars 👨‍💻✨⭐

---
+++ src/utils/getStars.js
@@ -0,0 +1,19 @@
+export default function getStars(course) {
+  let starsComplete = Math.trunc(course.stars);
+  let starsHalf = 0;
+  const decimal = course.stars - starsComplete;
+  let starsEmpty = 5 - starsComplete - (decimal ? 1 : 0);
+  if ((decimal && decimal < 0.5) || decimal === 0.5) starsHalf += 1;
+  else if (decimal) starsComplete += 1;
+  const stars = [];
+  for (let i = 1; i <= starsComplete; i++) {
+    stars.push("complete");
+  }
+  for (let i = 1; i <= starsHalf; i++) {
+    stars.push("half");
+  }
+  for (let i = 1; i <= starsEmpty; i++) {
+    stars.push("empty");
+  }
+  return stars;
+}
